24562fedafaf1e1495df1940b7e316332e1a4bd2
neriheredia <developer.heredia@gmail.com>
2022-05-20 00:04:56 -0300

    resolve problem and continue warng

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const a = "lamadiwm",
+  const a = "lamadiwm";
   return (
     <div className="App">
       <header className="App-header">
